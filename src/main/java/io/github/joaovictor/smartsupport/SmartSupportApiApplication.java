c5a5b54f7c7db61ec72d2983101f881b4867a3d6
JoaoAzevedo184 <ccjconrad50@gmail.com>
2026-06-27 16:20:55 -0300

    Initial commit

---
+++ src/main/java/io/github/joaovictor/smartsupport/SmartSupportApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaovictor.smartsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSupportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSupportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/joaovictor/smartsupport/SmartSupportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaovictor.smartsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSupportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
